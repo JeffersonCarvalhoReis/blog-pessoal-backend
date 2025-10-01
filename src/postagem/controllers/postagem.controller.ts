578b0a0b2ed962c4826ff49aa94783acc3846c1a
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-10-01 08:14:07 -0300

    Eslint

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
-import { PostagemService } from '../services/postagem.service';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from '../services/postagem.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('/postagens')
