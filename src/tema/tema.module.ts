7a0efd12c60ce9f414c1193a854261ee21c4ee9c
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-10-02 12:06:16 -0300

    Reorganiza a ordem das importações no módulo Tema

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TemaService } from './services/tema.service';
-import { TemaController } from './controllers/tema.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TemaController } from './controllers/tema.controller';
 import { Tema } from './entities/tema.entity';
+import { TemaService } from './services/tema.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tema])],
