5e81cac5df9856aa4337ed063f405dfedaadad73
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-10-07 08:51:04 -0300

    feat: organiza importações e inicializa o ConfigModule no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { ProdService } from './data/services/pro.service';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { ConfigModule } from '@nestjs/config';
-import { ProdService } from './data/services/pro.service';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useClass: ProdService,
       imports: [ConfigModule],
